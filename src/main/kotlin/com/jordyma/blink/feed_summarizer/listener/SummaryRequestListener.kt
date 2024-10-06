4327c48bae38494c044107b4a28ec5e5a249bc5a
dawn <misodaeun3@naver.com>
2024-10-06 22:16:21 +0900

    fix: summarizeFeed() return PromptResponse?

--- src/main/kotlin/com/jordyma/blink/feed_summarizer/listener/SummaryRequestListener.kt
+++ src/main/kotlin/com/jordyma/blink/feed_summarizer/listener/SummaryRequestListener.kt
@@ -1,6 +1,7 @@
 package com.jordyma.blink.feed_summarizer.listener
 
 import com.jordyma.blink.feed_summarizer.listener.dto.FeedSummarizeMessage
+import com.jordyma.blink.global.gemini.response.PromptResponse
 import io.awspring.cloud.sqs.listener.acknowledgement.Acknowledgement
 import org.springframework.messaging.Message
 import org.springframework.messaging.MessageHeaders
@@ -8,5 +9,5 @@ import org.springframework.messaging.handler.annotation.Headers
 
 interface SummaryRequestListener {
 
-    fun summarizeFeed(message: Message<FeedSummarizeMessage>, @Headers headers: MessageHeaders, acknowledgement: Acknowledgement): Unit
+    fun summarizeFeed(message: Message<FeedSummarizeMessage>, @Headers headers: MessageHeaders, acknowledgement: Acknowledgement): PromptResponse?
 }
--- src/main/kotlin/com/jordyma/blink/feed_summarizer/listener/SummaryRequestListenerImpl.kt
+++ src/main/kotlin/com/jordyma/blink/feed_summarizer/listener/SummaryRequestListenerImpl.kt
@@ -3,6 +3,7 @@ package com.jordyma.blink.feed_summarizer.listener
 import com.jordyma.blink.feed_summarizer.listener.dto.FeedSummarizeMessage
 import com.jordyma.blink.feed_summarizer.request_limiter.SummarizeRequestLimiter
 import com.jordyma.blink.feed_summarizer.service.FeedSummarizerService
+import com.jordyma.blink.global.gemini.response.PromptResponse
 import io.awspring.cloud.sqs.annotation.SqsListener
 import io.awspring.cloud.sqs.listener.acknowledgement.Acknowledgement
 import org.springframework.messaging.Message
@@ -17,7 +18,11 @@ class SummaryRequestListenerImpl(
 ) : SummaryRequestListener {
 
     @SqsListener("\${spring.cloud.aws.sqs.summary-request-queue.name}")
-    override fun summarizeFeed(message: Message<FeedSummarizeMessage>, @Headers headers: MessageHeaders, acknowledgement: Acknowledgement) {
+    override fun summarizeFeed(
+        message: Message<FeedSummarizeMessage>,
+        @Headers headers: MessageHeaders,
+        acknowledgement: Acknowledgement
+    ): PromptResponse? {
         // TOKEN이 남아있을 때에만 요청을 처리한다.
         if (summarizeRequestLimiter.decreaseToken() > 0) {
             // TODO 이 메서드에 요약 처리 로직을 추가해야함. 아래 메소드 구현 필요.
@@ -27,5 +32,6 @@ class SummaryRequestListenerImpl(
             // 정상적으로 요청을 처리한 경우에만 메세지큐에서 요청을 삭제한다.
             acknowledgement.acknowledge()
         }
+        return null
     }
 }
